aa4d9146d73c0982a0af5f8f16595f15dc55b35e
Josh Bennett <josh.bennett77@yahoo.com>
2022-03-31 17:11:16 -0600

    Write function to remove array items
    I wrote a function that filters through each item in an array and
    rewrites the array to include items of the array that are not
    strictly equal to the argument (the item to be removed from the array).

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...query) {
 
-};
+	array = array.filter(item => !query.includes(item))
+		return array;
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
