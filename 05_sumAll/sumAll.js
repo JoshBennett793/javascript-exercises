c8053481762ce8a8fcae6f13013d4077a6b2bca2
Josh Bennett <josh.bennett77@yahoo.com>
2022-04-01 12:06:55 -0600

    Write function that sums two arguments
    This function is meant to sum two arguments as well as every number
    in between both arguments. It returns an error message if the
    argument is a negative or non-numeric value. It works if the smaller
    number is passed as the first argument as well as the other way around.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
 
+//sums a + b as well as any number in between
+//if a = 1 and b = 4, sums: 1 + 2 + 3 + 4 = 10
+const sumAll = function(a, b) {
+	let finalSum = 0; 
+		
+	let i = a;
+	if (a < 0 || b < 0 ||
+			 isNaN(a) || isNaN(b) || 
+			 	typeof a === 'string' || 
+			 		typeof b === 'string') { // if argument passed is non-numeric or negative value
+		return 'ERROR';
+	} else if (a < b) {
+		do {
+	 		finalSum += i;
+			 i++;
+	} while (i <= b);
+}	else if (a > b) { // so it will work if the larger number is first argument passed
+		do {
+			finalSum += i;
+			i--;
+	} while (i >= b);
+}
+	return finalSum;
 };
 
 // Do not edit below this line
