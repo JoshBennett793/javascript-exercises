3423f8330a194e0dc97354a61c4a76c289d13698
Josh Bennett <josh.bennett77@yahoo.com>
2022-04-01 13:32:30 -0600

    Write function to determine if leap year
    This function takes one argument that is called into a few boolean
    comparison statements. If the argument is not at all divisible by 4,
    then it immediately returns false. If the argument is divisible by 4,
    divisible by 100, but not divisible by 400, returns false because that
    is not a leap year. All arguments that are divisible by 4 and 400 will
    return true.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+// A function to determine if a particular year
+// is a leap year. Returns true or false
+const leapYears = function(year) {
+	
+	if (year % 4 !== 0) {
+		return false;
+	}
+		else if (year % 400 !== 0 &&
+							year % 100 === 0) {
+		return false;
+	}
+		else {
+			return true;
+		}
 };
 
 // Do not edit below this line
