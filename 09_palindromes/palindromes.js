da0609e5b6e18c870bdcfc5697a449887b4df0e5
Josh Bennett <josh.bennett77@yahoo.com>
2022-05-24 17:25:01 -0600

    Passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = (string) => {
+	string = string.toLowerCase().replace(/[^a-zA-Z]/g, "").split("");
+	return string.join("") === string.reverse().join("") ? true : false;
 };
 
 // Do not edit below this line
