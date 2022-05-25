d1152c602e785804673ab4719eccc8f721684b47
Josh Bennett <josh.bennett77@yahoo.com>
2022-05-24 18:36:00 -0600

    Passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = (num) => {
+  if (num < 0) return "OOPS";
 
+  let a = 0,
+    	b = 1,
+    	temp;
+
+  while (num >= 0) {
+		temp = a;
+		a += b;
+		b = temp;
+		num--;
+	}
+
+	return b;
 };
 
 // Do not edit below this line
