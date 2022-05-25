079112dccbed2b001a6b2404376b7c3fd1998ecf
Josh Bennett <josh.bennett77@yahoo.com>
2022-05-25 15:35:58 -0600

    Passed all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people) => {
+  let currentYear = new Date().getFullYear();
+  return people
+    .map((person) => {
+      if (!person.yearOfDeath) {
+				person.age = currentYear - person.yearOfBirth;
+      } else {
+				person.age = person.yearOfDeath - person.yearOfBirth;
+      }
+      return person;
+    })
+    .sort((a, b) => {
+      return a.age > b.age ? -1 : 1;
+    })[0]
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
